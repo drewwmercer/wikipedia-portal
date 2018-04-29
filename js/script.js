9f2be5e92faed18617d54f48b1209f72a8577823
Drew Mercer <japanexpress@gmail.com>
2018-04-29 15:28:19 -0400

    Adding working search with results as li

--- js/script.js
+++ js/script.js
@@ -9,7 +9,11 @@ $(document).ready(function () {
             async: false,
             dataType: "json",
             success: function (data) {
-                console.log(data);
+                console.log(data[1][0]);
+                console.log(data[2][0]);
+                console.log(data[3][0]);
+                for (var i = 0; i < data[1].length; i++)
+                    $('#output').prepend("<li><a href="+data[3][i]+">"+data[1][i]+"</a><p>"+data[2][i]+"</p></li>");
             },
             error: function (errorMessage) {
                 alert("Error")
