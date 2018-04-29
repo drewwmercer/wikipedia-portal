454149a2450750165f85d48985e0b7702b978f6e
Drew Mercer <japanexpress@gmail.com>
2018-04-29 15:34:07 -0400

    Fixing to clear last output when returning results

--- js/script.js
+++ js/script.js
@@ -12,6 +12,9 @@ $(document).ready(function () {
                 console.log(data[1][0]);
                 console.log(data[2][0]);
                 console.log(data[3][0]);
+
+                $('#output').html('');
+
                 for (var i = 0; i < data[1].length; i++)
                     $('#output').prepend("<li><a href="+data[3][i]+">"+data[1][i]+"</a><p>"+data[2][i]+"</p></li>");
             },
